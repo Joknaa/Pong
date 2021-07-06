435a26427036b770199c1c228be2c20a54fe726f
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-07-06 20:53:46 +0100

    Added Easy, Hard, and Challenge difficulties

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -3,15 +3,30 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour {
     private GameObject GameMaster;
+    private Rigidbody2D BallRigidBody2D;
+    private Rigidbody2D SliderRigidBody2D;
+    private Vector2 BallVelocity;
+    private Vector2 SliderVelocity;
 
     private void Start() {
         GameMaster = GameObject.FindGameObjectWithTag("GameMaster");
+        BallRigidBody2D = gameObject.GetComponent<Rigidbody2D>();
+        BallVelocity = gameObject.GetComponent<Rigidbody2D>().velocity;
+        SliderRigidBody2D = GameObject.FindGameObjectWithTag("Slider").GetComponent<Rigidbody2D>();
+        SliderVelocity = GameObject.FindGameObjectWithTag("Slider").GetComponent<Rigidbody2D>().velocity;
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Goal")) {
             GameMaster.GetComponent<GameController>().RestartRound();
-            Debug.Log("GOAAAAAl");
+        }
+        else if (other.gameObject.CompareTag("Slider")) {
+            Vector2 direction = SliderRigidBody2D.position - BallRigidBody2D.position;
+            BallRigidBody2D.velocity += direction;
+
+            //BallRigidBody2D.AddForce(new Vector2(0,), ForceMode2D.Force);
+            //BallVelocity += SliderVelocity;
+            Debug.Log("tuch !!");
         }
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,12 +15,13 @@ public class GameController : MonoBehaviour {
     public void RestartRound() {
         Ball.transform.position = Vector3.zero;
         LaunchBall();
-
     }
 
     private void LaunchBall() {
         var Direction_X = math.sign(Random.value - 0.5f);
         var Direction_Y = math.sign(Random.value - 0.5f);
+        BallRigidbody2D.velocity = Vector2.zero;
+        //BallRigidbody2D.AddForce(new Vector2(200 * Direction_X, 200 * Direction_Y));
         BallRigidbody2D.AddForce(new Vector2(200 * Direction_X, 200 * Direction_Y));
     }
 }
