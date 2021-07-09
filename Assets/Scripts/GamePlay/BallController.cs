f5082c07669e2cd2f5c8a7fbe29d5b119f0f9797
Joknaa <54961980+Joknaa@users.noreply.github.com>
2021-07-09 17:11:14 +0100

    Made the ball speed increase over time

--- Assets/Scripts/GamePlay/BallController.cs
+++ Assets/Scripts/GamePlay/BallController.cs
@@ -1,15 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace GamePlay {
     public class BallController : MonoBehaviour {
+        private const int BASE_SPEED = 6;
         private Rigidbody2D BallRigidBody2D;
         private GameObject GameMaster;
-        private Rigidbody2D SliderRigidBody2D;
+        private int Speed = BASE_SPEED;
 
         private void Start() {
             GameMaster = GameObject.FindGameObjectWithTag("GameMaster");
             BallRigidBody2D = gameObject.GetComponent<Rigidbody2D>();
-            SliderRigidBody2D = GameObject.FindGameObjectWithTag("Slider").GetComponent<Rigidbody2D>();
         }
 
         private void OnTriggerEnter2D(Collider2D other) {
@@ -17,10 +18,15 @@ namespace GamePlay {
             if (other.gameObject.CompareTag("PlayerGoal") || other.gameObject.CompareTag("EnemyGoal")) {
                 GameMaster.GetComponent<ScoreController>().UpdateScore(OtherTag == "EnemyGoal");
                 GameMaster.GetComponent<GameController>().RestartRound();
+                Speed = BASE_SPEED;
             }
-            else if (other.gameObject.CompareTag("Slider")) {
-                BallRigidBody2D.velocity -= other.GetComponent<Rigidbody2D>().velocity;
-            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.CompareTag("Slider") || other.gameObject.CompareTag("EnemyAI")) {
+                Debug.Log(BallRigidBody2D.velocity);
+                BallRigidBody2D.velocity = Speed++ * BallRigidBody2D.velocity.normalized;
+            }        
         }
     }
 }
